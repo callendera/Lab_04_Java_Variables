6774b1d51c3bd6888966822b5aade1ab9048fc0e
callendera <acallender@live.sscc.edu>
2022-09-15 15:02:35 -0400

    created 7 ints and 7 doubles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int intOperandA = 3;
+        int intOperandB = 10;
+        int intSum;
+        int intProduct = 56;
+        int intDifference = 4;
+        int intQuotient = 66;
+        int intModulo = 87;
+
+        double doubleOperandA = 4.53;
+        double doubleOperandB = 7.23;
+        double doubleSum = 8.2;
+        double doubleProduct = 15.3;
+        double doubleDifference = 30.5;
+        double doubleQuotient = 19.1;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is: " + intSum);
     }
 }
